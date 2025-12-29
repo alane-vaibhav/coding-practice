028165b11a0cbfff2cef805344d24efbfb305f90
alane-vaibhav <alane.vaibhav@gmail.com>
2025-12-29 23:59:43 +0530

    Practice code

---
+++ index.js
@@ -0,0 +1,8 @@
+let min = 7;
+let max = 19;
+
+let output = Math.floor(Math.random() * (max - min + 1) + min);
+console.log(
+  Math.floor(Math.random() * (max - min + 1) + min),
+  Math.random() * 20
+);
